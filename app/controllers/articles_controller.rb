c0a686add8e4894f0194653df1a6eb5332bfb50b
cuachi <jagarfiasi@outlook.com>
2016-12-14 17:32:31 +0000

    Implement Showing Article Details

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -21,6 +21,10 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
   
+  def show
+    @article = Article.find(params[:id])
+  end
+  
   private 
   
   def article_params
--- spec/features/listing_article_spec.rb
+++ spec/features/listing_article_spec.rb
@@ -2,10 +2,8 @@ require "rails_helper"
 
 RSpec.feature "Listing Articles" do
   before do
-  @article1 = Article.create(title: "The first article",
-  body: "Lorem ipsum dolor sit amet, consectetur.") 
-  @article2 = Article.create(title: "The second article",
-  body: "Pellentesque ac ligula in tellus feugiat.") 
+  @article1 = Article.create(title: "The first article",  body: "Lorem ipsum dolor sit amet, consectetur.") 
+  @article2 = Article.create(title: "The second article",  body: "Pellentesque ac ligula in tellus feugiat.") 
 end
 
 scenario "A user lists all articles" do
@@ -32,6 +30,6 @@ scenario "A user has no articles" do
   within ("h1#no-articles") do
   expect(page).to have_content("No Articles Created")
   end
-end
+  end
 
 end
